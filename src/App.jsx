1d71ff00d74e80dba3ce398bb273a4517a9df0a1
vikascreate <vikaskumarsingh2018@gmail.com>
2023-01-24 00:17:55 +0530

    Background is now Controllable Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { useState } from 'react'
+
 import './App.css'
 import { Canvas } from '@react-three/fiber';
 import Box from './Components/Box';
 import { OrbitControls } from '@react-three/drei';
 import { Paper ,Box as BoxMUI} from '@mui/material';
 import NavBar from './Components/NavBar';
+import { useControls } from 'leva';
 
 function App() {
-
+const color=useControls('Color',{
+  value:'green'
+})
   return (
     <div className="App">
       <NavBar/>
-      <BoxMUI component='main' sx={{ p: 6 }}>
+      <BoxMUI component='main' sx={{ p: 6 ,position:'absolute',zIndex:1}}>
       <h1>This Is WebGl Experiments Shoe Project</h1>
       <h3>Contribute at github.com/vikascreate</h3>
-      <Paper elevation={16} sx={{height:'70vh',marginBottom:'10vh',marginLeft:'3vw',marginRight:'3vw'}}>
-        <Canvas camera={{ position: [0, 0, 3] }}>
-          <directionalLight position={[0,0,3]}/>
-          <directionalLight position={[0,0,-3]}/>
-          <Box position={[-1.5, 0, 0]} />
-          <Box position={[1.5, 0, 0]} />
-          <OrbitControls enableZoom={false} enablePan={false}/>
-        </Canvas>
+      <Paper elevation={16} sx={{height:'70vh',width:'80vw',marginBottom:'10vh',marginLeft:'3vw',marginRight:'3vw',opacity:'0.9'}}>
       </Paper>
       </BoxMUI>
+      <Canvas camera={{ position: [0, 0, 3] }} style={{height:'100%',position:'fixed'}}>
+          <directionalLight position={[0,0,3]}/>
+          <directionalLight position={[0,0,-3]}/>
+          <Box position={[-2, 0, 0]} color={color.value} />
+          <Box position={[2, 0, 0]} color={color.value}/>
+          <OrbitControls 
+          // enableZoom={false}
+           //enablePan={false}
+        />
+      </Canvas>
     </div>
   );
 }
--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -1,7 +1,8 @@
 import { useFrame } from "@react-three/fiber";
 import { useState,useRef } from "react";
-export default function Box(props){
+export default function Box({position,color}){
     const mesh = useRef()
+   // console.log(mesh.current.uuid)
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
     useFrame((_,delta)=>{
@@ -10,14 +11,14 @@ export default function Box(props){
     })
     return (
         <mesh
-        {...props}
+        position={position}
         ref={mesh}
         scale={active ? 1.5 : 1}
         onClick={(event) => setActive(!active)}
         onPointerOver={(event) => setHover(true)}
         onPointerOut={(event) => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshPhongMaterial color={hovered ? 'hotpink' : 'orange'} />
+        <boxGeometry args={[3, 3, 3]} />
+        <meshPhongMaterial color={color} />
       </mesh>
     )
 }

8ae6594a94a2b95f47a36b30f2b992be281d2afe
vikascreate <vikaskumarsingh2018@gmail.com>
2023-01-23 23:10:30 +0530

    Completed NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import { Canvas } from '@react-three/fiber';
 import Box from './Components/Box';
 import { OrbitControls } from '@react-three/drei';
-import { Paper } from '@mui/material';
+import { Paper ,Box as BoxMUI} from '@mui/material';
 import NavBar from './Components/NavBar';
+
 function App() {
 
   return (
     <div className="App">
       <NavBar/>
+      <BoxMUI component='main' sx={{ p: 6 }}>
       <h1>This Is WebGl Experiments Shoe Project</h1>
       <h3>Contribute at github.com/vikascreate</h3>
       <Paper elevation={16} sx={{height:'70vh',marginBottom:'10vh',marginLeft:'3vw',marginRight:'3vw'}}>
@@ -21,6 +23,7 @@ function App() {
           <OrbitControls enableZoom={false} enablePan={false}/>
         </Canvas>
       </Paper>
+      </BoxMUI>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,47 +2,67 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const navItems = ['Home', 'About', 'Contact'];
 function NavBar(props) {
 
     return (
       <div>
-        <Box sx={{ display: 'flex' }}>
-        <AppBar  component={'nav'}>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                  mr: 2,
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".3rem",
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
-              >
-                LOGO
-              </Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
+        <Box sx={{ display: "flex" }}>
+          <AppBar
+            component={"nav"}
+            color="transparent"
+            sx={{
+              backdropFilter: blur("15px"),
+              WebkitBackdropFilter: blur("15px"),
+              background: "rgba(255, 255, 255, .95)",
+              filter: blur("10px"),
+            }}
+          >
+            <Container maxWidth="xl">
+              <Toolbar disableGutters>
+                <AdbIcon
+                  sx={{
+                    display: { xs: "none", sm: "block", md: "flex" },
+                    mr: 1,
+                  }}
+                />
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/"
+                  sx={{
+                    mr: 2,
+                    display: { xs: "none", sm: "block", md: "flex" },
+                    flex: 1,
+                    fontFamily: "monospace",
+                    fontWeight: 700,
+                    letterSpacing: ".3rem",
+                    color: "inherit",
+                    textDecoration: "none",
+                  }}
+                >
+                  LOGO
+                </Typography>
+                <Box
+                  sx={{
+                    display: { xs: "none", sm: "block", md: "flex" },
+                    mr: 2,
+                  }}
+                >
+                  {navItems.map((item) => (
+                    <Button key={item} sx={{ color: "black" }}>
+                      {item}
+                    </Button>
+                  ))}
+                </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
         </Box>
       </div>
     );

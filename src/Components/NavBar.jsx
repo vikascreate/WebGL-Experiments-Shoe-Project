ab673a708baa4ace6e21d0d4fe686f0270f16ea1
vikascreate <vikaskumarsingh2018@gmail.com>
2023-01-24 20:45:12 +0530

    Adding Responsiveness for mobile devices

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+import { Button, IconButton ,Drawer,Divider,List,ListItem,ListItemButton,ListItemText} from '@mui/material';
 import Container from '@mui/material/Container';
 import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
 const navItems = ['Home', 'About', 'Contact'];
 function NavBar(props) {
-
+  const [mobileOpen,setMobileOpen]=React.useState(false)
+  const [openDrawer,setOpenDrawer]=React.useState(false)
+  useEffect(()=>{
+    if(window.innerWidth<480){
+      setMobileOpen(true)
+    }else{
+      setMobileOpen(false)
+    }
+  },[window.innerWidth])
+  function handleDrawerToggle(){
+    setOpenDrawer(!openDrawer)
+  }
+  function ShowcaseList(){
+    return (
+      <div>
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" ,width:'40vw'}}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            LOGO
+          </Typography>
+          <Divider />
+          <List>
+            {navItems.map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemButton sx={{ textAlign: "center" }}>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </div>
+    );
+  }
     return (
       <div>
         <Box sx={{ display: "flex" }}>
@@ -24,10 +57,24 @@ function NavBar(props) {
           >
             <Container maxWidth="xl">
               <Toolbar disableGutters>
+                {mobileOpen &&
+                  <>
+                    <IconButton onClick={handleDrawerToggle}>
+                      <MenuIcon />
+                    </IconButton>
+                    <Drawer
+                      open={openDrawer}
+                      onClose={() => setOpenDrawer(false)}
+                    >
+                      <ShowcaseList />
+                    </Drawer>
+                  </>
+                }
                 <AdbIcon
                   sx={{
-                    display: { xs: "none", sm: "block", md: "flex" },
-                    mr: 1,
+                    display: { xs: "flex", sm: "block", md: "flex" },
+                    mr: { md: "1" },
+                    ml: { xs: "30vw", sm: "0", md: "0" },
                   }}
                 />
                 <Typography
@@ -37,7 +84,7 @@ function NavBar(props) {
                   href="/"
                   sx={{
                     mr: 2,
-                    display: { xs: "none", sm: "block", md: "flex" },
+                    display: { xs: "flex", sm: "block", md: "flex" },
                     flex: 1,
                     fontFamily: "monospace",
                     fontWeight: 700,

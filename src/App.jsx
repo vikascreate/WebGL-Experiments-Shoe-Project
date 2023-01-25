7a5081d36a582987e5257c7db00bb57aa1b09ded
vikascreate <vikaskumarsingh2018@gmail.com>
2023-01-25 15:59:21 +0530

    Enable autorotate,Added Stats

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber';
 import Box from './Components/Box';
-import { OrbitControls } from '@react-three/drei';
 import { Paper ,Box as BoxMUI} from '@mui/material';
 import NavBar from './Components/NavBar';
 import { useControls } from 'leva';
@@ -17,7 +16,7 @@ const color=useControls('Color',{
       <BoxMUI component='main' sx={{ p: 6 ,zIndex:1}}>
       <h1>This Is WebGl Experiments Shoe Project</h1>
       <h3>Contribute at github.com/vikascreate</h3>
-      <Paper elevation={16} sx={{height:'70vh',width:'80vw',marginBottom:'10vh',marginLeft:'3vw',marginRight:'3vw',opacity:'0.93'}}>
+      <Paper elevation={16} sx={{height:'70vh',width:'80vw',marginBottom:'10vh',marginLeft:'3vw',marginRight:'3vw'}}>
         <Shoe/>
       </Paper>
       </BoxMUI>
@@ -26,10 +25,6 @@ const color=useControls('Color',{
           <directionalLight position={[0,0,-3]}/>
           <Box position={[-2, 0, 0]} color={color.value} />
           <Box position={[2, 0, 0]} color={color.value}/>
-          <OrbitControls 
-          // enableZoom={false}
-           //enablePan={false}
-        />
       </Canvas>
     </div>
   );
--- src/Components/Shoe.jsx
+++ src/Components/Shoe.jsx
@@ -2,16 +2,17 @@ import { Environment, OrbitControls } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import React from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
+import { Stats } from '@react-three/drei';
 function Shoe(props) {
     const gltf=useLoader(GLTFLoader,'./models/shoe.glb')
     return (
         <div style={{height:'100%'}}>
-           <Canvas camera={{position:[8,8,2]}}>
-            <Environment preset='forest' background/>
+           <Canvas camera={{position:[18,18,2]}}>
+            <Environment preset='dawn' background/>
             <primitive
             object={gltf.scene}/>
-            <OrbitControls/>
+            <OrbitControls autoRotate/>
+            <Stats/>
             </Canvas> 
         </div>
     );
